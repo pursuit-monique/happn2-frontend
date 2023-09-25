6c6d9a289026e022e1327ee1e600bfc305804de3
Monique <107360758+pursuit-monique@users.noreply.github.com>
2023-09-25 16:32:50 -0400

    Beta for conference

---
+++ src/components/New.jsx
@@ -0,0 +1,12 @@
+import NewEvent from "./NewEvent"
+
+
+export default function New(){
+
+
+return (<>
+     <NewEvent />
+    </>
+)
+
+}
--- src/components/UserMenu.jsx
+++ src/components/UserMenu.jsx
@@ -1,65 +1,75 @@
-import { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./UserMenu.css";
 
 const UserMenu = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
   useEffect(() => {
-    const dropdownToggle = document.getElementById("ImageDropdownToggle");
-    const dropdownMenu = document.getElementById("imageDropdownMenu");
-
-    dropdownToggle.addEventListener("click", () => {
-      dropdownMenu.classList.toggle("show");
-    });
-
-
-    document.addEventListener("click", (e) => {
-      if (
-        !dropdownToggle.contains(e.target) &&
-        !dropdownMenu.contains(e.target)
-      ) {
-        dropdownMenu.classList.remove("show");
+    const handleOutsideClick = (e) => {
+      if (!document.getElementById("ImageDropdownToggle").contains(e.target)) {
+        setIsDropdownOpen(false);
       }
-    });
+    };
+
+    document.addEventListener("click", handleOutsideClick);
+
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
   }, []);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
 
   return (
     <div className="dropdown">
-      <img
-        src="https://xsgames.co/randomusers/avatar.php?g=female"
-        className="Avatar"
-        alt="Avatar"
-        id="ImageDropdownToggle"
-        data-bs-toggle="dropdown" 
-      />
-      <ul
-        className="dropdown-menu dropdown-menu-dark UserMenu__bg"
-        aria-labelledby="ImageDropdownToggle"
-        id="imageDropdownMenu"
-      >
-        <li>
-          <Link to="/new" className="dropdown-item active">
-            New Event
+      <div className="position-relative">
+        <img
+          src="https://xsgames.co/randomusers/avatar.php?g=female"
+          className="Avatar"
+          alt="Avatar"
+          id="ImageDropdownToggle"
+          onClick={toggleDropdown}
+        />
+        <ul
+          className={`dropdown-menu dropdown-menu-dark UserMenu__bg ${
+            isDropdownOpen ? "show" : ""
+          }`}
+          aria-labelledby="ImageDropdownToggle"
+          style={{
+            position: "absolute",
+            top: '60px',
+            right: 0,
+            width: '200px'
+          }}
+        >
+          <li>
+            <Link to="/new" className="dropdown-item active">
+              New Event
             </Link>
-        </li>
-        <li>
-        <Link to="/user" className="dropdown-item" >
-            User Info
-          </Link>
-        </li>
-        <li>
-          <a className="dropdown-item" href="#">
-            Temporary
-          </a>
-        </li>
-        <li>
-          <hr className="dropdown-divider" />
-        </li>
-        <li>
-          <a className="dropdown-item" href="#">
-            Temporary
-          </a>
-        </li>
-      </ul>
+          </li>
+          <li>
+            <Link to="/user" className="dropdown-item">
+              User Info
+            </Link>
+          </li>
+          <li>
+            <a className="dropdown-item" href="#">
+              Temporary
+            </a>
+          </li>
+          <li>
+            <hr className="dropdown-divider" />
+          </li>
+          <li>
+            <a className="dropdown-item" href="#">
+              About
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
